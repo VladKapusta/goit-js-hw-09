fe649a5b8d087fad7c886ed9e6c78234320a513c
VladKapusta <vladkapysta96@gmail.com>
2022-12-24 01:31:10 +0200

    adds code for promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const refs = {
+  form: document.querySelector('.form'),
+};
+refs.form.addEventListener('input', onInputAddsValue);
+refs.form.addEventListener('submit', onSubmitFormCreatePromises);
+
+const formData = {};
+
+function onInputAddsValue(e) {
+  formData[e.target.name] = +e.target.value;
+}
+
+function onSubmitFormCreatePromises(e) {
+  e.preventDefault();
+  const { delay, step, amount } = formData;
+  let promiseDelay = delay;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    promiseDelay += step;
   }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.5;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
